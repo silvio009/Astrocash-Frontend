5abffedd8ef42cba4f40234171a40d3904e453bf
silvio009 <rm550821@fiap.com>
2025-07-30 12:27:17 -0300

    Melhorando o css do footer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -24,11 +24,15 @@ export default function Home() {
     { nome: "Ação 1", valor: "R$ 10,00", variacao: "+2%" },
     { nome: "ETF 2", valor: "R$ 20,00", variacao: "-1%" },
     { nome: "Cripto 3", valor: "R$ 15,50", variacao: "+0.5%" },
+    { nome: "Cripto 3", valor: "R$ 15,50", variacao: "+0.5%" },
+    { nome: "Cripto 3", valor: "R$ 15,50", variacao: "+0.5%" },
   ];
 
   const infoDoDia = [
   "Mercado em alta hoje.",
   "Bitcoin subiu 5%.",
+  "Ações da Petrobras estão estáveis.",
+  "Ações da Petrobras estão estáveis.",
   "Ações da Petrobras estão estáveis.",
 ];
 
@@ -36,29 +40,44 @@ export default function Home() {
   const criptomoedas = [
     { nome: "Bitcoin", preco: "R$ 120.000", variacao: "+5%" },
     { nome: "Ethereum", preco: "R$ 8.000", variacao: "-2%" },
+    { nome: "Ethereum", preco: "R$ 8.000", variacao: "-2%" },
+    { nome: "Ethereum", preco: "R$ 8.000", variacao: "-2%" },
+    { nome: "Ethereum", preco: "R$ 8.000", variacao: "-2%" },
   ];
   const acoes = [
     { nome: "Petrobras", preco: "R$ 30,00", variacao: "+1%" },
+    { nome: "Vale", preco: "R$ 60,00", variacao: "-0.5%" },
+    { nome: "Vale", preco: "R$ 60,00", variacao: "-0.5%" },
+    { nome: "Vale", preco: "R$ 60,00", variacao: "-0.5%" },
     { nome: "Vale", preco: "R$ 60,00", variacao: "-0.5%" },
   ];
   const etfs = [
     { nome: "IVVB11", preco: "R$ 110", variacao: "+0.3%" },
     { nome: "BOVA11", preco: "R$ 90", variacao: "-1%" },
+    { nome: "BOVA11", preco: "R$ 90", variacao: "-1%" },
+    { nome: "BOVA11", preco: "R$ 90", variacao: "-1%" },
+    { nome: "BOVA11", preco: "R$ 90", variacao: "-1%" },
   ];
   const stocks = [
     { nome: "Apple", preco: "$145", variacao: "+1.2%" },
+    { nome: "Google", preco: "$2.500", variacao: "+0.8%" },
+    { nome: "Google", preco: "$2.500", variacao: "+0.8%" },
+    { nome: "Google", preco: "$2.500", variacao: "+0.8%" },
+    { nome: "Google", preco: "$2.500", variacao: "+0.8%" },
     { nome: "Google", preco: "$2.500", variacao: "+0.8%" },
   ];
 
   return (
     <>
+
+      <div className="page-container">
       <Header toggleMenu={toggleMenu} menuOpen={menuOpen} />
       <Carousel />
       <SearchAndCards
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
         pequenosCards={pequenosCards}
-      />
+        />
       <MainSection
         criptomoedas={criptomoedas}
         acoes={acoes}
@@ -66,7 +85,8 @@ export default function Home() {
         stocks={stocks}
         searchTerm={searchTerm}
         infoDoDia={infoDoDia}
-      />
+        />
+        </div>
       <Footer />
     </>
   );
